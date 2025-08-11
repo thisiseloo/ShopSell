577d511c0892555c7828e13191de04d34390b4e8
thisiseloo <seltenetmemmedzadee@gmail.com>
2025-08-11 19:24:23 +0400

    commit

--- src/components/PromoOverlay.jsx/PromoOverlay.jsx
+++ src/components/PromoOverlay.jsx/PromoOverlay.jsx
@@ -1,12 +1,17 @@
+// 
+
+
+
+
 import React from "react";
 
 export default function PromoOverlay({ scrollToCategories }) {
   return (
-    <div className="relative w-full">
+    <div className="relative w-full pt-[50px] sm:pt-0">
       <img
         src="/images/promo.jpg"
         alt="Promo"
-        className="w-full h-[550px] object-cover"
+        className="w-full h-[700px] sm:h-[550px] object-cover"
       />
 
       {/* Overlay mətni */}
@@ -20,13 +25,13 @@ export default function PromoOverlay({ scrollToCategories }) {
         "
       >
         {/* Başlıq */}
-        <h2 className="text-gray-800 font-extrabold leading-tight text-[45px] max-w-[800px]">
+        <h2 className="text-gray-800 font-extrabold leading-tight text-[30px] sm:text-[45px] max-w-[800px]">
           ÜSLUBUNUZU TAMAMLAYAN <br /> AYAQQABI VƏ ÇANTARI <br />
           KƏŞF EDİN
         </h2>
 
         {/* Paraqraf */}
-        <p className="text-gray-800 mt-3 leading-relaxed text-[18px] max-w-[660px]">
+        <p className="text-gray-800 mt-3 leading-relaxed text-[14px] sm:text-[18px] max-w-[660px]">
           Zövqünüzə uyğun və fərdiliyinizi ön plana çıxaracaq şəkildə
           hazırlanmış, müxtəlif və incəliklə işlənmiş ayaqqabı və çantalarımızın
           geniş çeşidini nəzərdən keçirin.
@@ -34,28 +39,31 @@ export default function PromoOverlay({ scrollToCategories }) {
 
         {/* Düymə */}
         <div className="mt-3 flex justify-start">
-         <button
-  onClick={scrollToCategories}
-  className="w-[180px] h-[50px] bg-gray-800 text-white text-[18px] rounded-[40px] border-[1px] hover:!text-gray-900 hover:bg-gray-300 hover:border-gray-900 transition"
->
-  Alış-veriş edin
-</button>
-
+          <button
+            onClick={scrollToCategories}
+            className="w-[150px] h-[40px] sm:w-[180px] sm:h-[50px] bg-gray-800 text-white text-[16px] sm:text-[18px] rounded-[40px] border-[1px] hover:!text-gray-900 hover:bg-gray-300 hover:border-gray-900 transition"
+          >
+            Alış-veriş edin
+          </button>
         </div>
 
         {/* Stats */}
         <div className="mt-10 flex flex-wrap justify-start gap-x-20 gap-y-6 text-left">
           <div className="flex flex-col items-start max-w-[181px]">
-            <span className="text-gray-800 font-bold text-[35px]">200+</span>
-            <span className="text-gray-800 text-[18px]">Beynəlxalq Brendlər</span>
+            <span className="text-gray-800 font-bold text-[30px] sm:text-[35px]">200+</span>
+            <span className="text-gray-800 text-[14px] sm:text-[18px]">
+              Beynəlxalq Brendlər
+            </span>
           </div>
           <div className="flex flex-col items-start max-w-[260px]">
-            <span className="text-gray-900 font-bold text-[35px]">2,000+</span>
-            <span className="text-gray-900 text-[18px]">Yüksək Keyfiyyətli Məhsullar</span>
+            <span className="text-gray-900 font-bold text-[30px] sm:text-[35px]">2,000+</span>
+            <span className="text-gray-900 text-[14px] sm:text-[18px]">
+              Yüksək Keyfiyyətli Məhsullar
+            </span>
           </div>
           <div className="flex flex-col items-start max-w-[174px]">
-            <span className="text-gray-900 font-bold text-[35px]">30,000+</span>
-            <span className="text-gray-900 text-[18px]">Məmnun Müştərilər</span>
+            <span className="text-gray-900 font-bold text-[30px] sm:text-[35px]">30,000+</span>
+            <span className="text-gray-900 text-[14px] sm:text-[18px]">Məmnun Müştərilər</span>
           </div>
         </div>
       </div>
